1d09c555efbcaf3f1597755f8a15f7127b6c6e7b
fikrizulfikarb <fathur34rahman56@gmail.com>
2021-12-31 16:15:31 +0700

    Initial commit

---
+++ system/app/Models/produk.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+
+class produk extends Model {
+	protected $table = 'produk';
+
+	protected $casts = [
+		'created_at' => 'datetime',
+		'updated_at' => 'datetime',
+		'berat' => 'decimal:2'
+
+	];
+
+	function seller(){
+		return $this->belongsTo(User::class, 'id_user');
+	}
+
+	function getHargaAttribute(){
+		return  "Rp.".number_format($this->attributes['harga']);
+	}
+
+
+}
